d7120e93174504c3c2fb9a6bf92d94f5c841d9d4
suporno <sarkarsuporno36@gmail.com>
2021-05-24 14:20:09 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,63 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+require("dotenv").config()
+const feedRoutes = require("./routes/feed");
+const authRoutes = require('./routes/auth')
+const mongoose = require("mongoose");
+const app = express();
 
-const feedRoutes = require('../routes/feed.js')
-const app = express
+const MONGODB_URI = process.env.MONGODB_URI
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + "-" + file.originalname);
+  },
+});
 
-app.use('/feed', feedRoutes)
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
-app.listen(8080);
+app.use(bodyParser.json());
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+app.use('/auth', authRoutes)
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data
+  res.status(status).json({ message: message, data: data });
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,32 @@
-const route = require('color-convert/route')
-const express = require('express')
+const route = require("color-convert/route");
+const express = require("express");
+const { body } = require("express-validator");
+const isAuth = require('../middleware/is-auth')
+const feedController = require("../controllers/feed");
 
-const feedController = require('../controllers/feed')
+const router = express.Router();
 
-const router = express.Router()
+router.get("/posts", isAuth, feedController.getPosts);
 
-router.get('/posts', feedController.getPosts)
+router.post(
+  "/post", isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
+router.get("/post/:postId", feedController.getPost);
 
-module.exports = router
+router.put(
+  "/post/:postId", isAuth,
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete("/post/:postId", isAuth, feedController.deletePost);
+
+module.exports = router;
